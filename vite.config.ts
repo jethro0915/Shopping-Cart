aa4363520813f05bfbc139dc3e950be8b74aedfb
jethro <jethroku0915@hotmail.com>
2024-12-23 21:22:01 +1300

    "final change"

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/your-repository-name/",
+  base: "/Shopping-Cart/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
